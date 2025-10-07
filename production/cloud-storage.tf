a5eabdced304e90c044041833b50adfe70571c4f
Vikramjeet Singh <ervikramjeet@live.com>
2025-10-07 17:52:57 +0530

    Remove force_destroy from production bucket
    Removed force_destroy option from the storage bucket resource.

--- production/cloud-storage.tf
+++ production/cloud-storage.tf
@@ -1,7 +1,6 @@
 resource "google_storage_bucket" "production_bucket" {
   name          = var.bucket_name
   location      = var.region
-  force_destroy = true
 
   uniform_bucket_level_access = true
 
