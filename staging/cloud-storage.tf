7fa0c554aefd56b1a481854144736707ca61b15c
Vikramjeet Singh <ervikramjeet@live.com>
2025-10-07 17:52:27 +0530

    Update cloud storage bucket configuration
    Removed force_destroy and lifecycle_rule from the cloud storage bucket configuration.

--- staging/cloud-storage.tf
+++ staging/cloud-storage.tf
@@ -1,21 +1,11 @@
 resource "google_storage_bucket" "staging_bucket" {
   name          = var.bucket_name
   location      = var.region
-  force_destroy = true
 
   uniform_bucket_level_access = true
 
   versioning {
     enabled = true
-  }
-
-  lifecycle_rule {
-    action {
-      type = "Delete"
-    }
-    condition {
-      age = 30
-    }
   }
 
   cors {
